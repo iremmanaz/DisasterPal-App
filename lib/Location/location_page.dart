5253ca8134c3bba11b8eb07d40bb76aa9cafe1d8
İrem Manaz <iremmanaz@hotmail.com>
2024-06-03 21:31:48 +0300

    map

--- lib/Location/location_page.dart
+++ lib/Location/location_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class LocationPage extends StatefulWidget {
   @override
@@ -7,38 +10,72 @@ class LocationPage extends StatefulWidget {
 }
 
 class _LocationPageState extends State<LocationPage> {
-  Position? _currentPosition;
+  String _location = 'Unknown';
+  String _dateTime = '';
+  LatLng _currentLatLng = LatLng(0, 0);
 
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
+    _getLocation();
   }
 
-  Future<void> _getCurrentLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      setState(() {
-        _currentPosition = position;
-      });
-    } catch (e) {
-      print('Hata: $e');
+  Future<void> _getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
     }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _location = 'Lat: ${position.latitude}, Lon: ${position.longitude}';
+      _dateTime = DateFormat('yyyy-MM-dd – kk:mm').format(DateTime.now());
+      _currentLatLng = LatLng(position.latitude, position.longitude);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Location'),
+        title: Text('Location Page'),
       ),
-      body: Center(
-        child: _currentPosition != null
-            ? Text(
-                'Latitude: ${_currentPosition!.latitude}, Longitude: ${_currentPosition!.longitude}')
-            : CircularProgressIndicator(),
-      ),
+      body: content(),
     );
   }
+
+  Widget content() {
+    return FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(39.9334, 32.8597),
+          initialZoom: 11,
+          interactionOptions:
+              const InteractionOptions(flags: ~InteractiveFlag.doubleTapZoom),
+        ),
+        children: [
+          openStreetMapTileLayer,
+        ]);
+  }
+
+  TileLayer get openStreetMapTileLayer => TileLayer(
+        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+      );
 }
