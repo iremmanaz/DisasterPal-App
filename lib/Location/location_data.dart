3ab2ebb65eacd88f71a19f4f22d24f90460deebc
İrem Manaz <iremmanaz@hotmail.com>
2024-06-04 16:12:47 +0300

    location update

--- lib/Location/location_data.dart
+++ lib/Location/location_data.dart
@@ -1,11 +1,33 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class LocationData {
   final List<String> _locations = [];
+  final String _prefsKey =
+      'saved_locations'; // SharedPreferences anahtarını tanımlayın
 
-  void addLocation(String location) {
+  LocationData() {
+    _loadLocationsFromPrefs(); // Uygulama başladığında kayıtlı konumları yükle
+  }
+
+  Future<void> _loadLocationsFromPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? savedLocations = prefs.getStringList(_prefsKey);
+    if (savedLocations != null) {
+      _locations.addAll(savedLocations);
+    }
+  }
+
+  Future<void> addLocation(String location) async {
     if (_locations.length >= 20) {
       _locations.removeAt(0);
     }
-    _locations.insert(0, location); // En yeni konum en üste eklenir
+    _locations.add(location);
+    await _saveLocationsToPrefs(); // Konumu kaydedin
+  }
+
+  Future<void> _saveLocationsToPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList(_prefsKey, _locations);
   }
 
   List<String> getLocations() {
