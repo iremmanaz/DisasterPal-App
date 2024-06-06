c13012a2c3ded49926056ed4c721f61b6c2d5cd7
elifkarabay <74023147+elifkarabay@users.noreply.github.com>
2024-06-06 18:17:24 +0300

    Update result_page.dart

--- lib/Education/quiz/result_page.dart
+++ lib/Education/quiz/result_page.dart
@@ -3,31 +3,61 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
   final int score;
   final int total;
+  final List<String> questions;
+  final List<int> selectedAnswers;
+  final List<int> correctAnswers;
+  final List<List<String>> choices;
 
-  Result({required this.score, required this.total});
+  Result({
+    required this.score,
+    required this.total,
+    required this.questions,
+    required this.selectedAnswers,
+    required this.correctAnswers,
+    required this.choices,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Quiz Completed!',
-            style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            'Your score is $score out of $total',
-            style: TextStyle(fontSize: 24.0),
-          ),
-          SizedBox(height: 20.0),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('Restart Quiz'),
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz Result',
+            style: TextStyle(color: Colors.white)),
+        backgroundColor: const Color.fromARGB(255, 49, 49, 49),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: questions.length,
+          itemBuilder: (context, index) {
+            return Card(
+              color: selectedAnswers[index] == correctAnswers[index]
+                  ? Colors.green
+                  : Colors.red,
+              child: ListTile(
+                title: Text(questions[index]),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Your answer: ${choices[index][selectedAnswers[index]]}',
+                      style: TextStyle(
+                        color: selectedAnswers[index] == correctAnswers[index]
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                    ),
+                    if (selectedAnswers[index] != correctAnswers[index])
+                      Text(
+                        'Correct answer: ${choices[index][correctAnswers[index]]}',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
